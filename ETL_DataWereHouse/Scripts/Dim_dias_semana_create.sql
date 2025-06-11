818d56d72746fccbd59a9bb9b67799a797edeabc
George <juradoj28@gmail.com>
2025-06-10 23:21:15 -0500

    Update Dim_dias_semana_create.sql

--- ETL_DataWereHouse/Scripts/Dim_dias_semana_create.sql
+++ ETL_DataWereHouse/Scripts/Dim_dias_semana_create.sql
@@ -1,6 +1,9 @@
--- Crear la tabla 'usuarios' para almacenar la información básica de los usuarios registrados en el sistema.
--- Incluye ID único, nombre completo, correo electrónico y fecha de creación del registro.
+---- Explicación:
+-- OBJECT_ID('dbo.DIM_Dias_Semana', 'U'): Verifica si existe un objeto de tipo tabla ('U' para "User Table") con ese nombre.
+-- Si OBJECT_ID devuelve NULL, la tabla no existe, por lo que se ejecuta la instrucción CREATE TABLE.
 
+IF OBJECT_ID('dbo.DIM_Dias_Semana', 'U') IS NULL
+BEGIN
 	CREATE TABLE dbo.DIM_Dias_Semana (
 		DiaSemana tinyint IDENTITY(1,1) NOT NULL,
 		NomDiaSemana nvarchar(10) NOT NULL,
