4982e269cfcac77e0bfc796cde467af80a580515
George <juradoj28@gmail.com>
2025-06-11 11:23:10 -0500

    Update Fac_MasacresCOL_create.sql

--- ETL_DataWereHouse/Scripts/Fac_MasacresCOL_create.sql
+++ ETL_DataWereHouse/Scripts/Fac_MasacresCOL_create.sql
@@ -1,17 +1,18 @@
--- Crear la tabla 'usuarios' para almacenar la información básica de los usuarios registrados en el sistema.
--- Incluye ID único, nombre completo, correo electrónico y fecha de creación del registro.
+-- Explicación:
+-- OBJECT_ID('dbo.FAC_MasacresCOL', 'U'): Verifica si existe un objeto de tipo tabla ('U' para "User Table") con ese nombre.
+-- Si OBJECT_ID devuelve NULL, la tabla no existe, por lo que se ejecuta la instrucción CREATE TABLE.
 
-	CREATE TABLE dbo.FAC_Homicidios (
-		IdHomicidio int NOT NULL,
-		FechaHecho date NOT NULL,
-		CodMpio int NOT NULL,
-		IdZona tinyint NOT NULL,
-		IdSexo tinyint NOT NULL,
-		Cantidad tinyint NOT NULL,
-		CONSTRAINT FAC_Homicidios_PK PRIMARY KEY (IdHomicidio),
-		CONSTRAINT FAC_Homicidios_DIM_Fechas_FK FOREIGN KEY (FechaHecho) REFERENCES DataWareHouse.dbo.DIM_Fechas(FechaHecho),
-		CONSTRAINT FAC_Homicidios_DIM_Municipios_FK FOREIGN KEY (CodMpio) REFERENCES DataWareHouse.dbo.DIM_Municipios(CodMpio),
-		CONSTRAINT FAC_Homicidios_DIM_Sexos_FK FOREIGN KEY (IdSexo) REFERENCES DataWareHouse.dbo.DIM_Sexos(IdSexo),
-		CONSTRAINT FAC_Homicidios_DIM_Zonas_FK FOREIGN KEY (IdZona) REFERENCES DataWareHouse.dbo.DIM_Zonas(IdZona)
+IF OBJECT_ID('dbo.FAC_MasacresCOL', 'U') IS NULL
+BEGIN
+CREATE TABLE DataWereHouse.dbo.FAC_MasacresCOL (
+    IdMasacre INT IDENTITY(1,1) NOT NULL,
+    FechaHecho DATE NOT NULL,
+    CodMpio INT NOT NULL,
+    Casos TINYINT NOT NULL,
+    Victimas TINYINT NOT NULL,
+		CONSTRAINT FAC_MasacresCOL_PK PRIMARY KEY (IdMasacre),
+		CONSTRAINT FAC_MasacresCOL_DIM_Fechas_FK FOREIGN KEY (FechaHecho) REFERENCES dbo.DIM_Fechas(FechaHecho),
+		CONSTRAINT FAC_MasacresCOL_DIM_Municipios_FK FOREIGN KEY (CodMpio) REFERENCES dbo.DIM_Municipios(CodMpio),
+					
 	);
-END;
+	END;
