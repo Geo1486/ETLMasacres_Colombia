44e37ba5739c0ca99e8825836a96a17e75271a09
George <juradoj28@gmail.com>
2025-06-10 23:19:39 -0500

    Update Dim_departamentos_create.sql

--- ETL_DataWereHouse/Scripts/Dim_departamentos_create.sql
+++ ETL_DataWereHouse/Scripts/Dim_departamentos_create.sql
@@ -1,6 +1,9 @@
--- Crear la tabla 'usuarios' para almacenar la información básica de los usuarios registrados en el sistema.
--- Incluye ID único, nombre completo, correo electrónico y fecha de creación del registro.
+--- Explicación:
+-- OBJECT_ID('dbo.DIM_Departamentos', 'U'): Verifica si existe un objeto de tipo tabla ('U' para "User Table") con ese nombre.
+-- Si OBJECT_ID devuelve NULL, la tabla no existe, por lo que se ejecuta la instrucción CREATE TABLE.
 
+IF OBJECT_ID('dbo.DIM_Departamentos', 'U') IS NULL
+BEGIN
 	CREATE TABLE dbo.DIM_Departamentos (
 		CodDpto tinyint NOT NULL,
 		NomDpto nvarchar(60) NOT NULL,
